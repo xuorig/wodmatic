527fa4865765af51be9f183f03c1e36ba7b51e03
Marc-Andre Giroux <marc-andre.giroux2@usherbrooke.ca>
2015-04-09 19:31:46 -0400

    fix

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var scraper    = require('./scraper');
 
 mongoose.connect(process.env.MONGOLAB_URI); // connect to our database
 
